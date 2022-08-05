30e84d163effc7b43a31d7131c7ceec151005548
mattjamesdev <57205718+mattjamesdev@users.noreply.github.com>
2022-08-05 22:05:11 +1000

    Revert "Added HTTPS changes to home.js as well."
    This reverts commit 8b9389bf1804a06832e4417bdad79fa77bb963da.
    
    Don't need to add HTTPS stuff to home.js (turns out I only needed to
    rebuild the Docker image).

--- router/home.js
+++ router/home.js
@@ -1,15 +1,7 @@
 const express = require('express')
 const path = require('path');
-const https = require('https');
-const fs = require('fs');
 const app = express()
 //ejs
-
-const sslOptions = {
-  key: fs.readFileSync(path.join(__dirname, './certs/key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, './certs/cert.pem'))
-};
-
 app.engine('.html',require('ejs').__express)
 app.set('views',path.join(__dirname,'pages'))
 app.set('view engine','html')
@@ -21,7 +13,3 @@ app.get('/',(req,res)=>{
 
 
 app.listen(80)
-
-const httpsServer = https.createServer(sslOptions, app);
-
-httpsServer.listen(443);
