8b9389bf1804a06832e4417bdad79fa77bb963da
mattjamesdev <57205718+mattjamesdev@users.noreply.github.com>
2022-08-05 21:59:47 +1000

    Added HTTPS changes to home.js as well.

--- router/home.js
+++ router/home.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const path = require('path');
+const https = require('https');
+const fs = require('fs');
 const app = express()
 //ejs
+
+const sslOptions = {
+  key: fs.readFileSync(path.join(__dirname, './certs/key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, './certs/cert.pem'))
+};
+
 app.engine('.html',require('ejs').__express)
 app.set('views',path.join(__dirname,'pages'))
 app.set('view engine','html')
@@ -13,3 +21,7 @@ app.get('/',(req,res)=>{
 
 
 app.listen(80)
+
+const httpsServer = https.createServer(sslOptions, app);
+
+httpsServer.listen(443);
